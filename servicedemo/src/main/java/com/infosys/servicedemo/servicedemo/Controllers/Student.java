5d033c7d5ba2f095f1dcaaf1fab6904975a7c285
Vijayashankar Aruchamy <shankararu6@gmail.com>
2020-05-09 16:05:15 +0530

    changes for 2

--- servicedemo/src/main/java/com/infosys/servicedemo/servicedemo/Controllers/Student.java
+++ servicedemo/src/main/java/com/infosys/servicedemo/servicedemo/Controllers/Student.java
@@ -1,5 +1,8 @@
 package com.infosys.servicedemo.servicedemo.Controllers;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,18 +14,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/stu")
+@CrossOrigin("*")
 public class Student {
 
 	@GetMapping("/one")
 	@CrossOrigin("*")
-	public StudentModel getOneStudent() {
-		return new StudentModel("Priyanka", 1234567890, "priyanka@abc.com", ".net", "FullTime");
+	public ResponseEntity<?> getOneStudent() {
+		return new ResponseEntity<StudentModel>(new StudentModel("Priyanka", 1234567890, "priyanka@abc.com", ".net", "FullTime"),new HttpHeaders(),HttpStatus.OK);
+		// new StudentModel("Priyanka", 1234567890, "priyanka@abc.com", ".net", "FullTime");
 	}
 	@CrossOrigin("*")
 	@PostMapping(value = "/save")
-	public StudentModel saveStudentModel(@RequestBody StudentModel stu) {
+	public ResponseEntity<?> saveStudentModel(@RequestBody StudentModel stu) {
 		System.out.println(stu.toString());
-		return stu;
+		return new ResponseEntity<StudentModel>(stu,new HttpHeaders(),HttpStatus.OK);
+		// return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
 
 }
